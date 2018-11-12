243f7145f25b539cb416512d7b60f19c4bf39528
Saul Dominguez <saulydominguez@gmail.com>
2018-11-12 17:10:15 +0100

    get_files_and_dirs function created

--- watcher.py
+++ watcher.py
@@ -3,29 +3,30 @@ import time
 import socket
 import json
 
+def get_files_and_dirs(path):
+	_dirs_list = []
+	_files_list = []
+
+	for root, dirs, files in os.walk(path, topdown=False):
+		for _dir in dirs:
+			_dirs_list.append(os.path.join(root, _dir)[len(path) + 1:])
+		for f in files:
+			_files_list.append(os.path.join(root, f)[len(path) + 1:])
+
+	return (_files_list, _dirs_list)
+
 if __name__ == "__main__":
 	with socket.socket() as s:
 		s.connect(('10.10.1.6', 9000))
 
 		path_to_watch = "/home/saul/Desktop"
-
-		before_dirs = []
-		before_files = []
-		for root, dirs, files in os.walk(path_to_watch, topdown=False):
-			for _dir in dirs:
-				before_dirs.append(os.path.join(root, _dir)[len(path_to_watch) + 1:])
-			for f in files:
-				before_files.append(os.path.join(root, f)[len(path_to_watch) + 1:])
+		
+		before_files, before_dirs = get_files_and_dirs(path_to_watch)
 			
 		while True:
 			time.sleep (5)
-			after_dirs = []
-			after_files = []
-			for root, dirs, files in os.walk(path_to_watch, topdown=False):
-				for _dir in dirs:
-					after_dirs.append(os.path.join(root, _dir)[len(path_to_watch) + 1:])
-				for f in files:
-					after_files.append(os.path.join(root, f)[len(path_to_watch) + 1:])
+
+			after_files, after_dirs = get_files_and_dirs(path_to_watch)
 
 			added_dirs = ('add_dir', [f for f in after_dirs if not f in before_dirs])
 			removed_dirs = ('remove_dir', [f for f in before_dirs if not f in after_dirs])
@@ -34,15 +35,20 @@ if __name__ == "__main__":
 			removed_files = ('remove_file', [f for f in before_files if not f in after_files])
 			
 			if added_dirs[1]:
+				print(added_dirs)
 				s.send(json.dumps(added_dirs).encode(), 1024)
 			if removed_dirs[1]:
+				print(removed_dirs)
 				s.send(json.dumps(removed_dirs).encode(), 1024)
 			if added_files[1]:
+				print(added_files)
 				s.send(json.dumps(added_files).encode(), 1024)
 				for filename in added_files[1]:
 					with open(os.path.join(path_to_watch, filename), 'rb') as f:
 						s.sendfile(f)
+						pass
 			if removed_files[1]:
+				print(removed_files)
 				s.send(json.dumps(removed_files).encode(), 1024)
 			
 			before_dirs = after_dirs
